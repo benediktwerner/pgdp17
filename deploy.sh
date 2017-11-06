5fe116c4204570e35c9401c9b363ca24bc2e9499
Benedikt Werner <1benediktwerner@gmail.com>
2017-11-06 23:25:46 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 JEKYLL_ENV=production jekyll build
-scp * -r _site/* wernerbe@lxhalle.in.tum.de:home_page/html-data/pgdp/
+scp -r _site/* wernerbe@lxhalle.in.tum.de:home_page/html-data/pgdp/
